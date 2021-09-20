cdb18d254557a2521480e0eb95e47324e6ce6724
Brendan Morrissey <brendanmorrissey34@gmail.com>
2021-09-20 12:41:12 -0400

    Initial Commit

--- Develop/README.md
+++ Develop/README.md
@@ -1,7 +1,6 @@
-# Password-Generator
+# Password Generator
 
 ## Funtionality
 
 When the user clicks the "Generate Password" button, the first choose a number 8-128 characters, then they are guided through a series of prompts to determine whether or not the password will include uppercase, lowercase, special characters, or numbers. After completeing they will have a randomly generated password on screen that includes all of the options they chose.
 
-![Password_Gen](https://user-images.githubusercontent.com/88950762/134040169-502d3021-f067-4b87-a53f-d96e1fd61466.PNG)
---
+++ Develop/script.js
@@ -0,0 +1,63 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+var alphaLower = 'abcdefghijklmnopqrstuvwxyz'
+var alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var specialCharacter = "!#$%&'()*+,-./:;<=>?@][^_`{|}~"
+var number = '123456789'
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword()    
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+function generatePassword() {
+
+  var choices = ""
+  var passwordLength = prompt('How many characters would you like to use?', 'Enter a number between 8 and 128');
+  
+      if ( passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) ) {
+          alert('Please enter a number between 8 and 128');
+    return; 
+  }
+
+  var lowercaseConfirm = confirm('Would you like to use lowercase letters?');
+      if (lowercaseConfirm) {
+          choices += alphaLower;
+  }
+
+  var uppercaseConfirm = confirm('Would you like to use uppercase letters?');
+      if (uppercaseConfirm) {
+          choices += alphaUpper; 
+  }
+  
+  var numberConfirm = confirm('Would you like to use numbers?');
+      if (numberConfirm) {
+          choices += number; 
+  }
+  
+  var specialCharConfirm = confirm('Would you like to use special characters?');
+      if (specialCharConfirm) {
+          choices += specialCharacter;
+  }
+
+      if (!lowercaseConfirm && !uppercaseConfirm && !numberConfirm && !specialCharConfirm) {
+          alert('You must select at least one type of character!');
+  }
+
+  var passwordString = ''
+      for (i = 0; i <= passwordLength; i++) {
+          passwordString += choices.charAt(Math.floor(Math.random() * choices.length));
+  }
+  
+  return passwordString
+
+}
+  // Add event listener to generate button
+  
+generateBtn.addEventListener("click", writePassword);
